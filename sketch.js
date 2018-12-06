5de901e6007f200c7e1ced2f63fffacc83d8897a
vibravo <45087542+vibravo@users.noreply.github.com>
2018-12-06 13:46:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var bubble = {
 };
 
 var bubble2 = {
-    radius: 60,
+    radius: 20,
 };
 
 function setup() {
@@ -17,17 +17,17 @@ function draw() {
 }
 
 function display() {
-	for (var w = 0; w < windowWidth; w = w + 200){
-        for(var h = 0; h < windowHeight; h = h + 200){
-        fill(255-w/5,0,255-h/5);
-				ellipse(w,h,bubble.radius,bubble.radius);
+	for (var w = 0; w < windowWidth; w = w + 100){
+        for(var h = 0; h < windowHeight; h = h + 100){
+        	fill(255-w/5,0,255-h/5);
+		ellipse(w,h,bubble.radius,bubble.radius);
     		fill(255-h/5,0,255-w/5);
-        ellipse(w+100,h,bubble2.radius,bubble2.radius);
+       		ellipse(w+100,h,bubble2.radius,bubble2.radius);
    
         bubble.radius = bubble.radius + .04;
         bubble2.radius = bubble2.radius - .03
 
-        if (bubble.radius > 60){
+        if (bubble.radius > 20){
             bubble.radius = bubble.radius*-1;
             
         if (bubble2.radius < 10){
