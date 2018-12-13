418a1174dd0765c15b684436418ac6f85601576c
vibravo <45087542+vibravo@users.noreply.github.com>
2018-12-13 13:47:48 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,10 +22,10 @@ function display() {
         fill(200-w/3,0,200-h/8);
 				ellipse(w+20,h+20,bubble.radius,bubble.radius);
     		fill(255-h/2,0,255-w/8);
-        ellipse(w+80,h-40,bubble2.radius,bubble2.radius);
+        ellipse(w+80,h-20,bubble2.radius,bubble2.radius);
    
-        bubble.radius = bubble.radius + .03;
-        bubble2.radius = bubble2.radius - .03
+        bubble.radius = bubble.radius + .05;
+        bubble2.radius = bubble2.radius - .05;
 
         if (bubble.radius > 51){
             bubble.radius = bubble.radius*-1;
